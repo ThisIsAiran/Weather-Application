1b7bfb1c615164bbe2af8cc7d4721f36c2c1912e
Aman Gupta <guptaaman70659@gmail.com>
2020-01-03 10:38:16 -0500

    adding content

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,4 @@
 const request = require('request')
-
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/bd396944948acb34a21336c42707f5e4/' + latitude + ',' + longitude
     console.log(url);
@@ -10,8 +9,11 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             const Temp = parseInt(body.currently.temperature, 10)-32
+            const lowTemp = parseInt(body.daily.data[0].temperatureLow, 10)-32
+            const highTemp = parseInt(body.daily.data[0].temperatureHigh, 10)-32
             console.log(Temp)
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + Temp + ' Degree Celsius. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + Temp + ' Degree Celsius. The high today is ' +  highTemp +' with low of '+ lowTemp + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
